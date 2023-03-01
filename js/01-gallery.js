151a0121fb78f9765d839109588989f7620be3c1
Дмитрий Хоменко <dimkakhomenko@gmail.com>
2023-03-01 14:58:36 +0200

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+// вирішив попрактивуватись у створенні елементів, можу зробити і шаблонним рядком (по лінивому)) 
+const addGalleryItems = galleryItems.map((img, i) => {
+    const galleryItem = document.createElement('div');
+    galleryItem.classList.add('gallery__item');
+
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__link');
+    galleryLink.href = "large-image.jpg";
+
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add('gallery__image');
+    galleryImage.setAttribute('data-source', `${galleryItems[i].original}`);
+    galleryImage.src = `${galleryItems[i].preview}`;
+    galleryImage.alt = `${galleryItems[i].description}`;
+
+    galleryLink.append(galleryImage);
+    galleryItem.append(galleryLink);
+
+    return galleryItem;
+});
+galleryEl.append(...addGalleryItems);
+let instance;
+
+galleryEl.addEventListener('click', (e) => {
+    e.preventDefault()
+    window.addEventListener('keydown', closeImage)
+    if (e.target.classList.value === 'gallery__image') {
+        instance = basicLightbox.create(`<img src=${e.target.dataset.source} width="1280" height="852">`);
+        return instance.show();
+    }
+    instance = basicLightbox.create(`<img src=${e.target.firstElementChild.dataset.source} width="1280" height="852">`);
+    instance.show();
+});
+
+function closeImage(e) {
+    if (e.code === 'Escape' || e.code === 'Space' || e.code === 'Enter') {
+        instance.close();
+    };
+};
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const addGalleryItems = galleryItems.map(({ preview, original, description }) => `<a class="gallery__item gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`).join("");
+/*додав gallery__link до <a>, щоб працював ховер і фокус*/
+galleryEl.insertAdjacentHTML('beforeend', addGalleryItems);
+
+let gallery = new SimpleLightbox('.gallery__item', {
+    captions: true,
+    captionDelay: 250,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+});
+
+
+
